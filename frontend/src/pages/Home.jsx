59c240606a8624299edf6b8260cc1f609655715c
Lucas Lu <lucaslu@hotmail.com>
2023-01-17 13:04:59 -0800

    add fetching data feature

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react";
+
+import WorkoutDetails from "../components/WorkoutDetails";
+
 const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const res = await fetch("http://localhost:4000/api/workouts");
+      const data = await res.json();
+      console.log(data);
+
+      if (res.ok) {
+        setWorkouts(data);
+      }
+    };
+
+    fetchWorkouts();
+  }, []);
+
   return (
     <div className="home">
-      <h2>Home</h2>
+      <div className="workouts">
+        {workouts?.map((workout) => (
+          <WorkoutDetails key={workout._id} workout={workout} />
+        ))}
+      </div>
     </div>
   );
 };
