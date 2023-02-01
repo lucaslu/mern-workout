731fc7c6066fb9f0cb72fd18060815fc6808eb2a
Lucas Lu <lucaslu@hotmail.com>
2023-02-01 01:46:35 -0800

    add Auth Context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { WorkoutContextProvider } from "../src/context/WorkoutContext";
+import { AuthContextProvider } from "../src/context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <WorkoutContextProvider>
-      <App />
-    </WorkoutContextProvider>
+    <AuthContextProvider>
+      <WorkoutContextProvider>
+        <App />
+      </WorkoutContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
